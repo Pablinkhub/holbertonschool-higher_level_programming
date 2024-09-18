e29d2433968c9bee47cad72e36f1a05e8c685e82
Pablinkhub <espinola.pablo12@gmail.com>
2024-09-18 11:23:46 -0300

    Area Task

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for defining a Square class.
+"""
+
+
+class Square:
+    """A class that defines a square by its size with type and value checks.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Private attribute
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+    
