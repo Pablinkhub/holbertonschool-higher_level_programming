d8b5b7c80fceb27a0e01bc4131426285849208a2
Pablinkhub <espinola.pablo12@gmail.com>
2024-09-18 10:46:15 -0300

    Task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for defining a Square class.
+
+This module defines a Square class with a private size attribute.
+"""
+
+class Square:
+    """A class that defines a square by its size.
+
+    Attributes:
+        size (int): The size of the square, not verified for type or value.
+
+    Args:
+        size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size  # Private attribute
