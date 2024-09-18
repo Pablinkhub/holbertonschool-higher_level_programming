a884a7f87ca9c9ef0d9d8036839632981381c215
Pablinkhub <espinola.pablo12@gmail.com>
2024-09-18 11:00:54 -0300

    Dif Fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 """Module for defining a Square class.
-
-This module provides a simple Square class used for educational purposes.
 """
 
 
 class Square:
     """An empty class that defines a square.
-
-    This class serves as a foundation for future exercises which may require
-    a more detailed implementation of a square with properties and methods.
     """
     pass
