d2ecea8e5865599eb2e517f13955b534c891b4b4
Pablinkhub <espinola.pablo12@gmail.com>
2025-01-22 14:47:44 -0300

    NOW NOW NOW

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range (0, 100):
+    print('{:02}'.format(i - 1), end=', ')
     if i in range (99, 100):
         print('{:02}'.format(i), end='')
-    else:
-        print('{:02}'.format(i - 1), end=', ')
