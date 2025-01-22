d218474dd9de91e83367273010ebe5a93d2d48a5
Pablinkhub <espinola.pablo12@gmail.com>
2025-01-22 11:31:08 -0300

    now yes mayus

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0) * -1 and (number * -1 % 10 > 5):
-    print("last digit of",number, "is", number * -1 % 10, "and is greater than 5")
+    print("Last digit of",number, "is", number * -1 % 10, "and is greater than 5")
 if (number > 0) and (number % 10 > 5):
-    print("last digit of",number, "is", number % 10, "and is greater than 5")
+    print("Last digit of",number, "is", number % 10, "and is greater than 5")
 
 if (number > 0) and (number % 10 < 6 and number % 10 != 0):
-    print("last digit of",number, "is", number % 10, "and is less than 6 and not 0")
+    print("Last digit of",number, "is", number % 10, "and is less than 6 and not 0")
 if (number < 0) * - 1 and (number * -1 % 10 < 6  and number % 10 != 0):
-    print("last digit of",number, "is", number * -1 % 10, "and is less than 6 and not 0")
+    print("Last digit of",number, "is", number * -1 % 10, "and is less than 6 and not 0")
 
 if (number < 0) * -1 and (number * -1 % 10 == 0):
-    print("last digit of",number, "is", number * -1 % 10, " and is 0")
+    print("Last digit of",number, "is", number * -1 % 10, " and is 0")
 if (number > 0) and (number % 10 == 0):
-    print("last digit of",number, "is", number % 10, " and is 0")
+    print("Last digit of",number, "is", number % 10, " and is 0")
