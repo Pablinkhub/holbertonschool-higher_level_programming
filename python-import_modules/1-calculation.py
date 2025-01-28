72b1eb5895b58e53fd70b459fad9e8925256d093
Pablinkhub <espinola.pablo12@gmail.com>
2025-01-28 14:59:37 -0300

    a

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ def main():
     a = 10
     b = 5
     
-    print ("{} + {} = {}".format(A, b, add(a, b)))
-    print ("{} + {} = {}".format(A, b, sub(a, b)))
-    print ("{} + {} = {}".format(A, b, mul(a, b)))
-    print ("{} + {} = {}".format(A, b, div(a, b)))
+    print ("{} + {} = {}".format(a, b, add(a, b)))
+    print ("{} + {} = {}".format(a, b, sub(a, b)))
+    print ("{} + {} = {}".format(a, b, mul(a, b)))
+    print ("{} + {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     main()
