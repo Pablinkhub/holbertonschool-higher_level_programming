8334fd39b96eb2a665b53a6871777fac6fb6175b
Pablinkhub <espinola.pablo12@gmail.com>
2025-01-28 14:38:16 -0300

    "format"

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,8 +4,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    result = add(a,b)
-    print(f"{a} + {b} = {result}")
-    
-    if __name__ == "__main__":
-        main ()
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
