3663b6a4617320ddb6477732151af4114d8fe7dc
Pablinkhub <espinola.pablo12@gmail.com>
2025-01-29 12:42:18 -0300

    ??

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,6 +4,7 @@ def print_integers(lst):
         for item in lst:
             if not isinstance(item, int):
                 raise ValueError("Non-integer value found")
-            print("{0}".format(item))
+            print("{:d}".format(item))
     except ValueError as e:
         print("Error: {0}".format(e))
+
